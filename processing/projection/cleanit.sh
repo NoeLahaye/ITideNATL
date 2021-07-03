d4c33f7b8361d67178d3f28b9477054fb944490f
Noé Lahaye <noe.lahaye@inria.fr>
2021-07-03 19:08:10 +0200

    update cleanit

--- processing/projection/cleanit.sh
+++ processing/projection/cleanit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fils="outjob_*.[oe]* proj_*_ty-loop.[0-9]*.py"
+fils="outjob_*.[oe]* proj_*_ty-loop.[0-9]*.py job_proj_?_[0-9]*.sh"
 
 if [ $# -eq 1 ] && [ $1 = "rm" ]; then
     rm -f $fils
