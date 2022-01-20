20705a29250d0e1d18a85fcd0bf89ef98e1b32be
Noé Lahaye <noe.lahaye@inria.fr>
2022-01-20 16:26:28 +0100

    renaming

--- itidenatl/tools/plot.py
+++ itidenatl/tools/plot.py
@@ -1,19 +1,19 @@
 ### routine for preparing plots
-from matplotlib import pyplot as plt
+from matplotlib import pyplot 
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
-plot_kwgs = {"figsize":(12,8),
+_plot_kwgs = {"figsize":(12,8),
                  "central_lon":-40, "extent":[-95, 0, 6.5, 55]
                 }               
                  
 def prep_one_plot(**kwargs):
-    kwgs = plot_kwgs.copy()
+    kwgs = _plot_kwgs.copy()
     kwgs.update(kwargs)
     map_proj = ccrs.Robinson(central_longitude=kwgs["central_lon"])
 
-    fig = plt.figure(figsize=kwgs["figsize"])
-    ax = plt.axes(projection=map_proj)
+    fig = pyplot.figure(figsize=kwgs["figsize"])
+    ax = pyplot.axes(projection=map_proj)
     gl = ax.gridlines(draw_labels=True)
     gl.top_labels = False
     gl.right_labels = False
