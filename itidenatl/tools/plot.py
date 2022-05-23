a74e024f711f6181d0fd1785ff4e3e1f4c914005
Noé Lahaye <noe.lahaye@inria.fr>
2022-05-23 11:14:14 +0200

    add discrete cmap

--- itidenatl/tools/plot.py
+++ itidenatl/tools/plot.py
@@ -1,19 +1,23 @@
 ### routine for preparing plots
-from matplotlib import pyplot 
+import matplotlib as mpl
+from matplotlib import pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+
+import numpy as np
 
 _plot_kwgs = {"figsize":(12,8),
                  "central_lon":-40, "extent":[-95, 0, 6.5, 55]
                 }               
                  
+
 def prep_one_plot(**kwargs):
     kwgs = _plot_kwgs.copy()
     kwgs.update(kwargs)
     map_proj = ccrs.Robinson(central_longitude=kwgs["central_lon"])
 
-    fig = pyplot.figure(figsize=kwgs["figsize"])
-    ax = pyplot.axes(projection=map_proj)
+    fig = plt.figure(figsize=kwgs["figsize"])
+    ax = plt.axes(projection=map_proj)
     gl = ax.gridlines(draw_labels=True)
     gl.top_labels = False
     gl.right_labels = False
@@ -21,3 +25,32 @@ def prep_one_plot(**kwargs):
     ax.set_extent(kwgs["extent"])
 
     return fig, ax
+
+def get_discrete_cmap_norm(vmin=0, vmax=1, nlevs=10, cmap="Spectral", extend="neither"):
+    """ discrete colormap and linear norm
+
+    linear colorbar between vmin and vmax with "nlevs" levels using colormap "cmap".
+    Depending of the value of "extend", the colormap will have nlevs-1 (extend="neither"),
+    nlevs (extend="min" or "max") or nlevs+1 (extend="both") colors.
+
+    internally uses colors.from_levels_and_colors and pyplot.get_cmap from matplotlib
+
+    returns
+    _______
+    cmap, norm
+
+    """
+    cmap = plt.get_cmap(cmap)
+
+    if extend=="both":
+        idp = 1
+    elif extend=="neither":
+        idp = -1
+    else:
+        idp = 0
+
+    colors = cmap(np.linspace(0, 1, nlevs+idp))
+    levels = np.linspace(vmin, vmax, nlevs)
+
+    cmap, norm = mpl.colors.from_levels_and_colors(levels, colors, extend=extend)
+    return cmap, norm
